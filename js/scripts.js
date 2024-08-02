ca1f224e755a3e561a55ce1198b811a3d2fb8eed
Dipak Gohil <dipakgohil7273@gmail.com>
2024-08-02 14:30:19 +0530

    initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    var sidenav = document.getElementById("mySidenav");
+    sidenav.classList.add("open");
+}
+
+function closeNav() {
+    var sidenav = document.getElementById("mySidenav");
+    sidenav.classList.remove("open");
+}
